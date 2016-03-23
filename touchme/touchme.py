457a98012c45f59aee8e75751683fc809ff13e3a
Joey Stockermans <joestox@Joey.local>
2016-03-23 11:10:50 -0400

    yesss no more load function

--- touchme/touchme.py
+++ touchme/touchme.py
@@ -1,14 +1,12 @@
-from IPython.display import HTML, Javascript
-
-def load():
-    d3 = """<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.9/d3.js"></script>"""
-    d3sg = """<script src="https://rawgit.com/jstoxrocky/d3sg/master/d3sg.js"></script>"""
-    d3sg_style = """<link href="https://rawgit.com/jstoxrocky/d3sg/master/d3sg_style.css" type="text/css" rel="stylesheet">"""
-    return HTML(d3+d3sg+d3sg_style)
+from IPython.core.display import display, HTML, Javascript
 
 class chart():
     
     def __init__(self):
+        d3 = """<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.9/d3.js"></script>"""
+        d3sg = """<script src="https://rawgit.com/jstoxrocky/d3sg/master/d3sg.js"></script>"""
+        d3sg_style = """<link href="https://rawgit.com/jstoxrocky/d3sg/master/d3sg_style.css" type="text/css" rel="stylesheet">"""
+        display(HTML(d3+d3sg+d3sg_style))
         self.lines = []
 
     def render_js(self):
@@ -21,4 +19,5 @@ class chart():
         curr_line = """ch.line({x}, {y}, '{label}');""".format(x=x, y=y, label=label)
         self.lines.append(curr_line)
         return Javascript(self.render_js())
+    
     
